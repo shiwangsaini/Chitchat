cb0092997c4838a0755933a58b479e909e35a2f1
Shiwangsaini <sshiwang35@gmail.com>
2025-10-01 15:44:55 +0530

    bottom nevbar

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -7,19 +7,59 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+// main App
 class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    Center(child: Text("Home Screen")),
+    Center(child: Text("Chats Screen")),
+    Center(child: Text("Settings Screen")),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Title(color: Colors.black, child: Text("Chit Chat")),
-          backgroundColor: Color.fromARGB(100, 100, 100, 100),
+          title: const Text("Chit Chat"),
+          backgroundColor: const Color.fromARGB(100, 100, 100, 100),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                debugPrint("button pressed");
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
         ),
-        body: Center(
-          child: Text("materialApp"),
+
+        body: _screens[_currentIndex],
+
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            debugPrint("Tapped index: $index");
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.verified_user),
+              label: "Chats",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
